d384c57eb619fd42ab195f3569bb336a17b1c37e
赵安东 <zhaoandong4213@sina.com>
2020-10-19 13:59:07 +0800

    2020/10/19

--- pdd/src/app/shared/directives/grid-item-title.directive.ts
+++ pdd/src/app/shared/directives/grid-item-title.directive.ts
@@ -1,15 +1,22 @@
-import { Directive, OnInit, Input, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Input,
+  ElementRef,
+  Renderer2,
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
   selector: '[appGridItemTitle]',
 })
 export class GridItemTitleDirective implements OnInit {
-  @Input() appGridItemTitle = '0.5rem';
+  @HostBinding('style.font-size') @Input() appGridItemTitle = '0.5rem'; // (装饰器，注解)可以写多个，来修饰一个变量
   constructor(private elr: ElementRef, private renderer: Renderer2) {}
   ngOnInit(): void {
     // 声明自己占据模版中的 title 区块
     this.setStyle('grid-area', 'title');
-    this.setStyle('font-size', this.appGridItemTitle);
+    // this.setStyle('font-size', this.appGridItemTitle);
   }
 
   private setStyle(styleName: string, styleValue: string | number) {
--- pdd/src/app/shared/directives/grid-item.directive.ts
+++ pdd/src/app/shared/directives/grid-item.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  OnInit,
+  HostBinding,
+} from '@angular/core';
 
 /**
  * 指令可以理解为没有模版的组件，它需要一个宿主元素。
@@ -9,13 +15,17 @@ import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
   selector: '[appGridItem]',
 })
 export class GridItemDirective implements OnInit {
+  // HostBinding 一个装饰器，用于把一个 DOM 属性标记为绑定到宿主的属性，并提供配置元数据。 Angular 在变更检测期间会自动检查宿主属性绑定，如果这个绑定变化了，它就会更新该指令所在的宿主元素。
+  @HostBinding('style.color') color = 'blue';
+  @HostBinding('style.display') display = 'grid';
+  @HostBinding('style.grid-template-areas') area = `'image' 'title'`;
   constructor(private elr: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.setStyle('display', 'grid');
+    // this.setStyle('display', 'grid');
 
     // 给出网格的模版，默认情况下是一个堆叠的布局，给出一个网格的两个构成部分的命名: image 和 title
-    this.setStyle('grid-template-areas', `'image' 'title'`);
+    // this.setStyle('grid-template-areas', `'image' 'title'`);
     this.setStyle('place-items', 'center');
     this.setStyle('width', '4.3rem');
   }
