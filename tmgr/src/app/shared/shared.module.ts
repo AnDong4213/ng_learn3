2d31159e31169a251f1ee7b311fa77a1cd9fd455
赵安东 <zhaoandong4213@sina.com>
2020-12-14 15:58:43 +0800

    theme

--- tmgr/src/app/shared/shared.module.ts
+++ tmgr/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatListModule,
   ],
   exports: [
     CommonModule,
@@ -23,6 +25,7 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatListModule,
   ],
   declarations: [],
 })
--- tmgr/src/app/utils/svg.util.ts
+++ tmgr/src/app/utils/svg.util.ts
@@ -1,9 +1,45 @@
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import * as _ from 'lodash';
 
 export const loadSvgResources = (ir: MatIconRegistry, ds: DomSanitizer) => {
+  const imgDir = 'assets/img';
+  const avatarDir = `${imgDir}/avatar`;
+  const sidebarDir = `${imgDir}/sidebar`;
+  const iconDir = `${imgDir}/icons`;
+  const dayDir = `${imgDir}/days`;
+
   ir.addSvgIcon(
     'ships',
     ds.bypassSecurityTrustResourceUrl('assets/container_ship.svg')
   );
+  ir.addSvgIcon(
+    'day',
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`)
+  );
+  ir.addSvgIcon(
+    'month',
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`)
+  );
+  ir.addSvgIcon(
+    'project',
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`)
+  );
+  ir.addSvgIcon(
+    'projects',
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`)
+  );
+  ir.addSvgIcon(
+    'week',
+    ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`)
+  );
+
+  const days = _.range(1, 32);
+  // console.log('days', days);
+  days.forEach((day) => {
+    ir.addSvgIcon(
+      `day${day}`,
+      ds.bypassSecurityTrustResourceUrl(`${dayDir}/day${day}.svg`)
+    );
+  });
 };
